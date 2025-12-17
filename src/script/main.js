7615d3bd88b2385e00a37bedb90b4dcede2f92e0
teuaraujoo <empresarial.mateus25@gmail.com>
2025-12-17 14:43:48 -0300

    howitworks + education section
    aplicacao do scroll smooth e finalizacao da sessao 3 e inicio da sessao 4

--- src/script/main.js
+++ src/script/main.js
@@ -1,9 +1,11 @@
 import { fadeInHero } from './fadeInHero.js';
 import { fadeInAbout } from './fadeInAbout.js';
 import { fadeInHowItWorks } from './fadeInHowItWorks.js';
+import { scrollSmooth } from './scrollSmooth.js';
 
 document.addEventListener("DOMContentLoaded", function() {
     fadeInHero();
     fadeInAbout();
     fadeInHowItWorks();
+    scrollSmooth();
 });
